83c5ef6e80c5d3be0d4d9dfdaa56c10abbdeca95
Ben Benyamin <benbin52+git@gmail.com>
2025-05-27 23:29:50 -0500

    Added DDP

--- train.py
+++ train.py
@@ -13,13 +13,34 @@ from optimizer import CosineSchedulerWithWarmup
 from loss import GPT2Loss
 
 from torch.utils.tensorboard import SummaryWriter
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
+
+import os
 
 writer = SummaryWriter(log_dir="runs/gpt2")
 torch.set_float32_matmul_precision('high')
+# Set seed so each agent has the same model at first
+torch.manual_seed(1337)
+torch.cuda.manual_seed(1337)
+
+AGENT_NUM = int(os.environ.get('LOCAL_RANK', 0))
+TOTAL_AGENTS = int(os.environ.get('WORLD_SIZE', 1))
+IS_DDP = TOTAL_AGENTS > 1
+MASTER_PROCESS = AGENT_NUM == 0
+
+
+if IS_DDP:
+    dist.init_process_group(backend="nccl")
+    torch.cuda.set_device(AGENT_NUM)
+
+def log(str):
+    if (MASTER_PROCESS): print(str)
+
+log(f"Number of agents = {TOTAL_AGENTS}")
 
 
 LR = 6e-4
-
 N_EMBD = 768
 N_BLOCK = 12
 N_HEADS = 12
@@ -30,29 +51,32 @@ HEAD_DIM = N_EMBD // N_HEADS
 
 TOKEN_PER_BATCH = 524288
 BATCH_SIZE  = 32
-BATCH_ITER = TOKEN_PER_BATCH // (1024 *BATCH_SIZE)
+BATCH_ITER = TOKEN_PER_BATCH // (1024 *BATCH_SIZE *TOTAL_AGENTS)
 EPOCHS = 1
 
 # Load dataset
-train_dataset = FineWebEdu("train", agent_num=1, n_chunks=24//2)
+train_dataset = FineWebEdu("train", agent_num=AGENT_NUM, n_chunks=24//TOTAL_AGENTS)
 val_dataset = FineWebEdu("val")
 
 train_loader = DataLoader(
     train_dataset,
     batch_size=BATCH_SIZE,
     shuffle= True,
+    pin_memory=True
 )
 
 val_loader = DataLoader(
     val_dataset,
     batch_size=BATCH_SIZE,
+    pin_memory=True
 )
+
 
 
 LR_WARMUP_STEPS = 375e6 // TOKEN_PER_BATCH
 TOTAL_STEPS = len(train_loader) // BATCH_ITER * EPOCHS
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+DEVICE = f'cuda:{AGENT_NUM}' if IS_DDP else 'cuda' if torch.cuda.is_available() else 'cpu'
 
 model = GPT2(
     n_blocks=N_BLOCK,
@@ -62,6 +86,10 @@ model = GPT2(
     vocab_size=VOCAB_SIZE,
     dropout=DROPOUT
 ).to(DEVICE)
+
+if (IS_DDP):
+    
+    model = DDP(model, device_ids=[AGENT_NUM])
 
 model = torch.compile(model)
 
@@ -77,10 +105,11 @@ loss_crit = GPT2Loss()
 
 scaler = GradScaler('cuda')
 
+model.train()
+optimizer.zero_grad()
+
 for epoch in range(EPOCHS):
 
-    model.train()
-    optimizer.zero_grad()
     acc_loss = 0.0
     start_time = time.time() *1000
 
@@ -93,6 +122,9 @@ for epoch in range(EPOCHS):
             loss = loss_crit(logits, targets) / BATCH_ITER 
 
         acc_loss += loss.item() # remove from comp graph
+        if (IS_DDP):
+            model.require_backward_grad_sync = ((batch_idx + 1) % BATCH_ITER == 0)
+
         scaler.scale(loss).backward()
 
         #Grad accumulation
@@ -102,16 +134,20 @@ for epoch in range(EPOCHS):
             scaler.step(optimizer)
             scaler.update()
             optimizer.zero_grad()
-
             elapsed = (time.time()*1000) - start_time 
             total_tokens = tokens.numel() *BATCH_ITER
             tokens_per_sec = total_tokens / (elapsed/1000)
-            print(
+            log(
                 f"EPOCH {epoch} Batch {(batch_idx+1)//BATCH_ITER}/{len(train_loader)//BATCH_ITER}  "
                 f"Loss: {acc_loss:.4f}  Norm: {norm:.2f}  LR: {optimizer.lr:.4e}  "
                 f"Step Time: {elapsed:.1f} ms  {tokens_per_sec:.0f} tok/s"
             )
-            writer.add_scalar("Train Loss",acc_loss,batch_idx*(epoch+1))
-            writer.add_scalar("Tokens/Sec",tokens_per_sec,batch_idx*(epoch+1))
+            if (MASTER_PROCESS):
+                writer.add_scalar("Train Loss",acc_loss,batch_idx*(epoch+1))
+                writer.add_scalar("Tokens/Sec",tokens_per_sec,batch_idx*(epoch+1))
+                writer.add_scalar("Learning Rate", optimizer.lr,batch_idx*(epoch+1))
             start_time = time.time() *1000
             acc_loss = 0.0
+
+if IS_DDP:
+    dist.destroy_process_group()
